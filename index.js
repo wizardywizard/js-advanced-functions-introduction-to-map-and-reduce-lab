a9db219fc1799a015c17441638fd9efa736784c9
wizardywizard <littledavid175@gmail.com>
2020-02-26 19:55:20 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Your code here
+let numbers = [1, 2, 3, -9]
+
+const mapToNegativize = () =>{
+    return numbers.map(number => (number * -1))
+}
+
+const mapToNoChange = (dune) =>{
+    return dune.map(dun => (dun))
+}
+
+const mapToDouble = () =>{
+    return numbers.map(number =>(number * 2))
+}
+
+const mapToSquare = () =>{
+    return numbers.map(number => (number * number))
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0 ){
+    let num = startingPoint
+   for(let i = 0; i < sourceArray.length; i++){
+     num += sourceArray[i]}
+     return num     
+}
+
+function reduceToAllTrue(sourceArray){
+    for(let i = 0; i < sourceArray.length; i++){
+        if (!sourceArray[i])
+        return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(sourceArray){
+    for(let i = 0; i < sourceArray.length; i++){
+        if (sourceArray[i])
+        return true
+    }
+    return false
+}
